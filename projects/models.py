62fda3707ad81547c6f2fd47179dff8af49d39d1
amir dwikat <amirdwikat@gmail.com>
2025-04-24 10:04:24 +0200

    making models(category, project, task, User)

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.conf.global_settings import AUTH_USER_MODEL
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.title
+
+class ProjectStatus(models.IntegerChoices):
+    PENDING = 1, 'Pending'
+    COMPLETED = 2, 'Completed'
+    POSTPONED = 3, 'Postponed'
+    CANCELED  = 4, 'Canceled' 
+
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    status = models.IntegerChoices(choices=ProjectStatus.choices, default=ProjectStatus.PENDING)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    user = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE, null=True)
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class Task(models.Model):
+    description = models.TextField()
+    is_completed = models.BooleanField(default=False)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.description
+
+
+
